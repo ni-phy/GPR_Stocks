3987fce1a04459f213c82cca35075bf87f8a853e
ni-phy <nxh700@student.bham.ac.uk>
2025-04-07 14:17:03 +0300

    kernel and spare data improvements

--- gp_for_stocks.py
+++ gp_for_stocks.py
@@ -9,27 +9,32 @@ from sklearn import gaussian_process as gp
 from sklearn.gaussian_process.kernels import DotProduct, RBF, WhiteKernel
 
 start_date = '2021-01-01'
-end_date = '2024-12-31'
+end_date = '2025-01-31'
 index = 'AAPL'  # Example stock symbol, change as needed
 # Get the stock data
 stock = yf.download(index, start=start_date, end=end_date)
 
 data = pd.DataFrame.to_numpy(stock['Close'])
 data = data-data[0]
+sparse_data = data[:800:20]  # Downsample the data
+sparse_days = np.atleast_2d(np.arange(len(data))[:800:20]).T
 days = np.atleast_2d(np.arange(len(data))).T
 
 # Create the Gaussian Process model
-kernel = DotProduct()+RBF(length_scale=1e-3) + WhiteKernel(noise_level=0.1)
-model = gp.GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
+kernel = 0.3*DotProduct()+0.3*RBF(length_scale=1e-2)  + 0.1*WhiteKernel(
+    noise_level=1e-2, noise_level_bounds=(1e-10, 1e1))
+model = gp.GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=100)
 # Fit the model to the data
-model.fit(days, data)
+model.fit(sparse_days, sparse_data)
 # Make predictions
 predictions = model.predict(days, return_std=True)
+print(predictions)
 # Get the dates for the x-axis
 dates = pd.date_range(start=start_date, end=end_date)
 # Plot the results
 plt.figure(figsize=(12, 6))
 plt.plot(days, data, 'r.', markersize=10, label='Observed Data')
+plt.plot(sparse_days, sparse_data, 'bx', markersize=10, label='Training Data')
 plt.plot(days, predictions[0], 'b-', label='GP Mean')
 plt.fill_between(np.arange(len(data)), predictions[0] - 1.96 * predictions[1], predictions[0] + 1.96 * predictions[1], alpha=0.2, color='blue', label='95% Confidence Interval')
 plt.title(f'Gaussian Process Regression for {index}')
