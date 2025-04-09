61270dc8eb0cf3006cd31b17d85ba89157c3ee55
ni-phy <nxh700@student.bham.ac.uk>
2025-04-09 21:57:37 +0300

    added moving averages

--- gp_for_stocks.py
+++ gp_for_stocks.py
@@ -8,8 +8,20 @@ import sklearn as sk
 from sklearn import gaussian_process as gp
 from sklearn.gaussian_process.kernels import DotProduct, RBF, WhiteKernel
 
-def trailing_avg(arr):
-    return sum(arr)/len(arr)
+def SMA(arr, n=10):
+    """Compute the moving average of an array."""
+    res = [sum(arr[i-n:i])/n for i in range(n, len(arr))]
+    return res 
+
+def sparse(arr, n=10, s=2):
+    """Compute the sparse average of an array."""
+    l = len(arr)
+    res = []
+    avg_ind = []
+    for i in range(0, l-s*n, n):
+        res.append(arr[i])
+        avg_ind.append(i+n)
+    return res, avg_ind
 
 start_date = '2021-01-01'
 end_date = '2025-01-31'
@@ -19,11 +31,15 @@ stock = yf.download(index, start=start_date, end=end_date)
 
 data = pd.DataFrame.to_numpy(stock['Close'])
 data = data-data[0]
-
-# Downsample the data, -2 to ensure there is unsampled data for prediction
-sparse_data = [trailing_avg(data[10*(i-1):10*i]) for i in range(1,len(data)//10-2)]  
-sparse_days = np.atleast_2d(np.arange(len(sparse_data))*10).T
 days = np.atleast_2d(np.arange(len(data))).T
+
+#find the moving average
+day_avg = 25
+SMA_data = SMA(data, n=day_avg)
+
+# Downsample the data, s ensures there is unsampled data for prediction
+sparse_data, sparse_days = sparse(SMA_data, n=day_avg, s=1)
+sparse_days = np.atleast_2d(sparse_days).T
 
 # Create the Gaussian Process model
 kernel = 0.3*DotProduct()+0.3*RBF(length_scale=1e-2)  + 0.1*WhiteKernel(
@@ -33,20 +49,17 @@ model = gp.GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=100)
 model.fit(sparse_days, sparse_data)
 # Make predictions
 predictions = model.predict(days, return_std=True)
-print(predictions)
-# Get the dates for the x-axis
-dates = pd.date_range(start=start_date, end=end_date)
+
 # Plot the results
 plt.figure(figsize=(12, 6))
 plt.plot(days, data, 'r.', markersize=10, label='Observed Data')
-plt.plot(sparse_days, sparse_data, 'bx', markersize=10, label='Training Data')
+plt.plot(sparse_days, sparse_data, 'gx', markersize=10, label='Training Data')
 plt.plot(days, predictions[0], 'b-', label='GP Mean')
+plt.plot(days[day_avg:], SMA_data, 'g-', label='SMA')
 plt.fill_between(np.arange(len(data)), predictions[0] - 1.96 * predictions[1], predictions[0] + 1.96 * predictions[1], alpha=0.2, color='blue', label='95% Confidence Interval')
 plt.title(f'Gaussian Process Regression for {index}')
 plt.xlabel('Date')
 plt.ylabel('Stock Price')
 plt.legend()
-# plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-# plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
 plt.gcf().autofmt_xdate() 
 plt.show()
